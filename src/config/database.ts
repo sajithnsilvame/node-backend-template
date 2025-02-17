3c3c5671069a23d95e754a85bd28b06680158f07
sajithnsilvame <sajithnsilva.me@gmail.com>
2025-02-17 14:11:21 +0530

    changes to be committed:
        modified:   .env
        modified:   src/config/database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,13 +9,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD as string, 
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),  // <- Ensure port is used
     dialect: 'mysql',
     logging: false,
     pool: {
-      max: 5, // Maximum number of connection in pool
-      min: 0, // Minimum number of connection in pool
-      acquire: 30000, // The maximum time, in milliseconds, that pool will try to get connection before throwing error
-      idle: 10000 // The maximum time, in milliseconds, that a connection can be idle before being released
+      max: 5, // Maximum number of connections in pool
+      min: 0, // Minimum number of connections in pool
+      acquire: 30000, // The maximum time Sequelize will try to get connection before throwing an error
+      idle: 10000 // The maximum time a connection can be idle before being released
     }
   }
 );
